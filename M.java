7e4eac4765d395014e7e329f4690008dc9c7e1a9
PARTHA4030 <114592496+PARTHA4030@users.noreply.github.com>
2022-09-30 20:45:09 +0530

    Add files via upload

---
+++ M.java
@@ -0,0 +1,19 @@
+ class M
+{ 
+    public static void Add()
+    {
+        int a=10,b=20;
+        System.out.print("Sum:-"+(a+b));
+    }
+    public static void main(String[]args)
+    { 
+      M r=new M();
+      r.Disp();
+      M.Add();
+    }
+     void Disp()
+     {
+        System.out.print(" Learning Code");
+     }
+    
+}
