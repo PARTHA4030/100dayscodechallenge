fe890089ac77a92105787f226abe312514447449
PARTHA PRATIM MONDAL <114592496+PARTHA4030@users.noreply.github.com>
2022-10-24 22:51:13 +0530

    Add files via upload

---
+++ day8_JAVA(While Loop)24-10-2022/Solution.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+      
+        Scanner r=new Scanner(System.in);
+        
+        int i=1;
+       while(r.hasNext()){
+        System.out.println(i+" "+r.nextLine());
+        i++; 
+       }   
+    }   
+}
