a5621b02f18a1eca07da8697c7b5f54713acdbe8
PARTHA PRATIM MONDAL <114592496+PARTHA4030@users.noreply.github.com>
2022-11-04 23:23:50 +0530

    day15Progress

---
+++ day0015_JAVA(BigInteger)04-11-2022/Solution.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner r = new Scanner(System.in);
+
+    BigInteger A = r.nextBigInteger();
+    BigInteger B = r.nextBigInteger();   
+    System.out.println(A.add(B));
+    System.out.println(A.multiply(B));
+
+    }
+}
