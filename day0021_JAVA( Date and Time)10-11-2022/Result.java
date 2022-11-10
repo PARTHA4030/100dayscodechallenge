ee6fceeb73059a59d7fbb32f8cbb58dc5ed98ce0
PARTHA PRATIM MONDAL <114592496+PARTHA4030@users.noreply.github.com>
2022-11-10 20:22:44 +0530

    day0021Progress

--- day0021_JAVA( Date and Time)10-11-2022/Result.java
+++ day0021_JAVA( Date and Time)10-11-2022/Result.java
@@ -5,7 +5,6 @@ import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
-
 class Result {
     public static String findDay(int month, int day, int year) {
        Calendar cal = Calendar.getInstance();
@@ -18,20 +17,13 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
         String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
         int month = Integer.parseInt(firstMultipleInput[0]);
-
         int day = Integer.parseInt(firstMultipleInput[1]);
-
         int year = Integer.parseInt(firstMultipleInput[2]);
-
         String res = Result.findDay(month, day, year);
-
         bufferedWriter.write(res);
         bufferedWriter.newLine();
-
         bufferedReader.close();
         bufferedWriter.close();
     }
