c4d90b1aff7651fba87ca921e552e9c6c69fb870
PARTHA PRATIM MONDAL <114592496+PARTHA4030@users.noreply.github.com>
2022-12-27 23:03:39 +0530

    day0046Progress

---
+++ day0046_JAVA_(Java Comparator)27-12-2022/Player.java
@@ -0,0 +1,52 @@
+import java.util.*;
+class Checker implements Comparator<Player>{
+    public int compare(Player o1, Player o2){
+        if(o1.score < o2.score){
+            return 1;
+        }
+        else if(o1.score > o2.score){
+            return -1;
+        }
+        else{
+            if(o1.name.compareTo(o2.name) <= -1){
+                return -1;
+            }
+            else if (o1.name.compareTo(o2.name) >= 1){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+    }
+}
+class Player{
+    String name;
+    int score;
+    
+    Player(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+        
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+     
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
