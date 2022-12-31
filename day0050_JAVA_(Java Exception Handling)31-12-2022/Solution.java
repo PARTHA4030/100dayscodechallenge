00ecce64abef05ea6eb24fbf59cb828edee669c3
PARTHA PRATIM MONDAL <114592496+PARTHA4030@users.noreply.github.com>
2022-12-31 09:48:54 +0530

    day0050Progress

---
+++ day0050_JAVA_(Java Exception Handling)31-12-2022/Solution.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+class MyCalculator {
+    public int power(int n, int p) throws Exception
+    {
+        if(n == 0 && p == 0) 
+        {
+            throw new Exception("n and p should not be zero.");
+        }
+        else if(n < 0 || p < 0)
+        {
+            throw new Exception("n or p should not be negative.");
+        }
+        else
+        {
+            int result = 1;
+            for(int i = 1; i <= p; i++)
+            {
+                result = result * n;
+            }
+            
+            return result; 
+        }    
+        
+    }
+}
+
+public class Solution {
+    public static final MyCalculator my_calculator = new MyCalculator();
+    public static final Scanner in = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        while (in .hasNextInt()) {
+            int n = in .nextInt();
+            int p = in .nextInt();
+            
+            try {
+                System.out.println(my_calculator.power(n, p));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
