38cd170273e88b5dc85a30b5f7f9b0607cb68869
PARTHA PRATIM MONDAL <114592496+PARTHA4030@users.noreply.github.com>
2023-02-13 19:42:43 +0530

    day0063Progress

---
+++ day0063_JAVA_(Tree Preorder Traversal)13-02-2023/Solution.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+public static void preOrder(Node root) {
+        class Traverse{
+            public Traverse(Node node){
+                System.out.print(node.data + " ");
+                if(node.left != null){
+                    new Traverse(node.left);
+                }
+                if(node.right != null){
+                    new Traverse(node.right);
+                }
+            }
+        }
+        new Traverse(root);
+    }
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        preOrder(root);
+    }	
+}
