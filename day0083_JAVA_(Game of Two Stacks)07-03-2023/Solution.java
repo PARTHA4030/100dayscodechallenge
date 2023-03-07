17ae3bae1366d2de429f125d029e228f1093415b
PARTHA PRATIM MONDAL <114592496+PARTHA4030@users.noreply.github.com>
2023-03-07 18:34:27 +0530

    day0083Progress

---
+++ day0083_JAVA_(Game of Two Stacks)07-03-2023/Solution.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static int twoStacks(int maxSum, List<Integer> a, List<Integer> b) {
+    int result =0,s1Count=0,st2Count=0,totalSum=0;
+     for (Integer s1Element : a){
+         if(totalSum+s1Element >maxSum)break;
+         totalSum +=s1Element;
+         s1Count++;
+     }
+      result = s1Count;
+      for(Integer s2Element:b){
+          totalSum += s2Element;
+          st2Count++;
+          while(totalSum >maxSum && s1Count > 0){
+              totalSum -=a.get(s1Count-1);
+              s1Count--;
+          }
+          result = (totalSum <= maxSum)?Math.max(s1Count + st2Count , result):result;
+      }
+      return result;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int g = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, g).forEach(gItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int m = Integer.parseInt(firstMultipleInput[1]);
+
+                int maxSum = Integer.parseInt(firstMultipleInput[2]);
+
+                List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                int result = Result.twoStacks(maxSum, a, b);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
