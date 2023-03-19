640ab50d2ead4d9f87d50521d9a47b7dfedc5aac
PARTHA PRATIM MONDAL <114592496+PARTHA4030@users.noreply.github.com>
2023-03-20 00:03:13 +0530

    day0096Progress

---
+++ day0096_JAVA_(Apple and Orange)20-03-2023/Solution.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+    public static void countApplesAndOranges(int s, int t, int a, int b, List<Integer> apples, List<Integer> oranges) {
+    int appleCounter = 0;
+        int orangeCounter = 0;
+        
+        for(int apple : apples){
+            if(apple + a >= s && apple + a <= t) appleCounter++;
+        }        
+        for(int orange : oranges){
+            if(orange + b >= s && orange + b <= t) orangeCounter++;
+        }        
+        System.out.println(appleCounter);
+        System.out.println(orangeCounter);       
+    } 
+    
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int s = Integer.parseInt(firstMultipleInput[0]);
+
+        int t = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int a = Integer.parseInt(secondMultipleInput[0]);
+
+        int b = Integer.parseInt(secondMultipleInput[1]);
+
+        String[] thirdMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int m = Integer.parseInt(thirdMultipleInput[0]);
+
+        int n = Integer.parseInt(thirdMultipleInput[1]);
+
+        String[] applesTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> apples = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            int applesItem = Integer.parseInt(applesTemp[i]);
+            apples.add(applesItem);
+        }
+
+        String[] orangesTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> oranges = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int orangesItem = Integer.parseInt(orangesTemp[i]);
+            oranges.add(orangesItem);
+        }
+
+        Result.countApplesAndOranges(s, t, a, b, apples, oranges);
+
+        bufferedReader.close();
+    }
+}
